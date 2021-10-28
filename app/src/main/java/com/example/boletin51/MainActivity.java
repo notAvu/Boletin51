18f35dbab87a2dffa2349a46c462a48cbd49af1d
Álvaro Fernández <avunotfound@gmail.com>
2021-10-28 09:56:07 +0200

    Añadida lista de equipos

--- app/src/main/java/com/example/boletin51/MainActivity.java
+++ app/src/main/java/com/example/boletin51/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.boletin51;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import clases.Equipo;
 
@@ -27,6 +36,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NbaAdapter<Equipo> adapter= new NbaAdapter<Equipo>(this, R.id.list_item ,nbaTeams);
+    }
+
+    public class ViewHolder
+    {
+        private TextView tv;
+        private ImageView img;
+
+        public TextView getTv() {
+            return tv;
+        }
+
+        public void setTv(TextView tv) {
+            this.tv = tv;
+        }
+
+        public ImageView getImg() {
+            return img;
+        }
+
+        public void setImg(ImageView img) {
+            this.img = img;
+        }
+
+        public ViewHolder(TextView tv, ImageView img) {
+            this.tv = tv;
+            this.img = img;
+        }
+    }
+
+    public class NbaAdapter<T> extends ArrayAdapter
+    {
+        public NbaAdapter(@NonNull Context context, int resource, int textViewResourceId, @NonNull Object[] objects) {
+            super(context, resource, textViewResourceId, objects);
+        }
+        public NbaAdapter(@NonNull Context context, int resource,  @NonNull Object[] objects) {
+            super(context, resource, objects);
+        }
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+        {
+            View view = super.getView(position, convertView, parent);
+
+            LayoutInflater inflater= getLayoutInflater();
+            inflater.inflate(R.layout.custom_list_layout, parent, false);
+            TextView text= findViewById(R.id.list_item);
+            ImageView iv=findViewById(R.id.image);
+            ImageView image = view.findViewById(R.id.image);
+            TextView item = view.findViewById(R.id.list_item);
+            item.setText(nbaTeams[position].toString());
+            image.setImageResource(nbaTeams[position].getIdEscudo());
+
+            return view;
+        }
+
+
     }
 
 }
