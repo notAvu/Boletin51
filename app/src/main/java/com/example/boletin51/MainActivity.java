7f916f5f607e084449272f49fe39ea4095b0132a
Álvaro Fernández <avunotfound@gmail.com>
2021-11-09 08:50:42 +0100

    LOG

--- app/src/main/java/com/example/boletin51/MainActivity.java
+++ app/src/main/java/com/example/boletin51/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,13 @@ import android.widget.TextView;
 import clases.Equipo;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String LOG_TAG="Actividad principal";
+    String LOG_ONCREATE="Se ha entrado en el metodo onCreate";
+    String LOG_GETVIEW="Ejecutando metodo getView del NbaAdapter";
+    String LOG_ONPAUSE="La actividad ha entrado en pausa";
+    String LOG_ONSAVEINSTANCESTATE="Lo que diga Miguel";
+    String LOG_ONRESTORE="Restaurado";
 
     Equipo[] nbaTeams={
 
@@ -40,10 +48,37 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i(LOG_TAG, LOG_ONCREATE);
+
         Spinner sp= findViewById(R.id.spinTeams);
         NbaAdapter<Equipo> adapter= new NbaAdapter<>(this, R.id.list_item ,nbaTeams);
         sp.setAdapter(adapter);
         sp.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.i(LOG_TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, LOG_ONPAUSE);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(LOG_TAG, LOG_ONSAVEINSTANCESTATE);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.w(LOG_TAG,LOG_ONRESTORE);
     }
 
     @Override
